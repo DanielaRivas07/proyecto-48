13ec348f6f4178a9fe2aa6e6f7e7eaa00380d6f2
Daniela Rivas <garuana.09@gmail.com>
2023-11-18 12:06:25 -0300

    cambio js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const navEl = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
         navEl.classList.add('navbar-scrolled')
-    }else if(window.screenY < 56){
+    } else if (window.screenY < 56) {
         navEl.classList.remove('navbar-scrolled')
     }
 });
@@ -13,28 +13,28 @@ const navEla = document.querySelector('nav');
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
         navEla.classList.add('nav-scrolled')
-    }else if(window.screenY < 56){
+    } else if (window.screenY < 56) {
         navEla.classList.remove('nav-scrolled')
     }
 });
 
 const swith = document.querySelector(".switch");
 
-swith.addEventListener("click", e =>{
+swith.addEventListener("click", e => {
     swith.classList.toggle("active");
     document.body.classList.toggle("active")
 });
 
 const heart = document.querySelector("footer.main span[title='<3']");
-  let beat;
-  heart.addEventListener("mousedown", () => {
+let beat;
+heart.addEventListener("mousedown", () => {
     if (!beat)
-      beat = new Audio("img/corazonlatiendo.mp3");
+        beat = new Audio("img/corazonlatiendo.mp3");
     beat.currentTime = 0;
     beat.play();
-  });
+});
 
-  function agrandarTexto(elemento) {
+function agrandarTexto(elemento) {
     elemento.classList.add('agrandado');
-    elemento.removeEventListener('click', agrandarTexto); 
-  }
+    elemento.removeEventListener('click', agrandarTexto);
+}
